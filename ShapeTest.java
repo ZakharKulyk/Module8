0df149a2a87122ff9f157f94e0098d32e125b23f
zkhr <zakhar864@gmail.com>
2024-03-25 11:58:25 +0100

    HomeworkModule8

---
+++ ShapeTest.java
@@ -0,0 +1,12 @@
+package Homework.Module8;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        DisplayName displayName = new DisplayName();
+      System.out.println(displayName.showData(circle1));
+
+
+    }
+
+}
